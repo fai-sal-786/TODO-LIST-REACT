60eda539eb5e925b9f092475db310954268c9612
Abdul-0006 <154364633+Abdul-0006@users.noreply.github.com>
2024-12-08 18:48:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import REACT, { useState } from 'react'
+import TodoList from './components/Todo-list'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TodoList/>
+    </>
+  )
+}
+
+export default App;
